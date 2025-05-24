f790dd233da6cb96187290b9118242493de9de73
Harish <129611646+Harish3000@users.noreply.github.com>
2025-05-24 16:27:50 +0530

    update API

--- next-app/convex/myAction.ts
+++ next-app/convex/myAction.ts
@@ -6,45 +6,48 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const ingest = action({
-    args: {
-        splitText: v.array(v.string()),
-        fileId : v.string(),
+  args: {
+    splitText: v.array(v.string()),
+    fileId: v.string(),
   },
   handler: async (ctx, args) => {
     await ConvexVectorStore.fromTexts(
       args.splitText,
-     { fileId: args.fileId }, 
+      { fileId: args.fileId },
       new GoogleGenerativeAIEmbeddings({
-      apiKey: 'AIzaSyD01IOfTQfBBpgPvYT0YCU_cAVKJGwPOSs',
-      model: "text-embedding-004", // 768 dimensions
-      taskType: TaskType.RETRIEVAL_DOCUMENT,
-      title: "Document title",
-    }),
+        apiKey: "AIzaSyCgkJhM2g7diEVKanhfqeVNtxf04hba0LY",
+        model: "text-embedding-004", // 768 dimensions
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+      }),
       { ctx }
     );
     console.log("Ingest action completed for fileId:", args.fileId);
-        return "Completed";
+    return "Completed";
   },
 });
 
 export const search = action({
   args: {
     query: v.string(),
-    fileId:v.string()
+    fileId: v.string(),
   },
   handler: async (ctx, args) => {
     const vectorStore = new ConvexVectorStore(
       new GoogleGenerativeAIEmbeddings({
-      apiKey: 'AIzaSyD01IOfTQfBBpgPvYT0YCU_cAVKJGwPOSs',
-      model: "text-embedding-004", // 768 dimensions
-      taskType: TaskType.RETRIEVAL_DOCUMENT,
-      title: "Document title",
-      }), { ctx });
-    
-    const resultOne = await (await vectorStore.similaritySearch(args.query, 1)).filter(q => q.metadata.fileId === args.fileId);
-   console.log("Search results for query:", args.query, "Results:", resultOne);
+        apiKey: "AIzaSyCgkJhM2g7diEVKanhfqeVNtxf04hba0LY",
+        model: "text-embedding-004", // 768 dimensions
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+      }),
+      { ctx }
+    );
+
+    const resultOne = await (
+      await vectorStore.similaritySearch(args.query, 1)
+    ).filter((q) => q.metadata.fileId === args.fileId);
+    console.log("Search results for query:", args.query, "Results:", resultOne);
 
     return JSON.stringify(resultOne);
   },
 });
-
